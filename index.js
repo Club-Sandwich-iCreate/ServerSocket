3d3ad4f5277c5689baf66b23ef60c5b964552a14
Félix Arnoult <felix.arnoult@etu.univ-nantes.fr>
2018-03-27 12:22:26 +0200

    Add all socket.on & socket.emit methods

--- index.js
+++ index.js
@@ -5,19 +5,63 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log(socket.id);
-    socket.on('update', () => {
-        console.log('emiting');
-        io.emit('update');
-    });
+  console.log(socket);
+  socket.on('update', () => {
+    console.log('emiting');
+    io.emit('update');
+  });
+
+  socket.on('startApplication', function(socket, msg) {
+    console.log("ON - startApplication " + msg);
+  });
+
+  socket.on('checkID', function(socket, msg) {
+    console.log("ON - checkID " + msg);
+  });
+
+  socket.on('startAmination', function(socket, msg) {
+    console.log("ON - startAmination " + msg);
+  });
+
+  socket.on('soundEvent', function(socket, msg) {
+    console.log("ON + soundEvent " + msg);
+  });
+
+  socket.on('playSong', function(socket, msg) { //quand le master ordonne de démarrer les sons
+    console.log("ON + playSong " + msg);
+  });
+
+  socket.on('soundMatch', function(socket, msg) {
+    console.log("ON + soundMatch " + msg);
+  });
+
+  socket.on('stopSong', function(socket, msg) {
+    console.log("ON + stopSong " + msg);
+  });
+
+  socket.on('end', function(socket, msg) {
+    console.log("ON + end " + msg);
+  });
+  //socket.emit('startApplication', '');
+  //socket.emit('checkID', '');
+  //socket.emit('startAnimation', '');
+  //socket.emit('soundEvent', '');
+  //socket.emit('playSong', '');
+  //socket.emit('soundMatch', '');
+  //socket.emit('stopSong', '');
+  //socket.emit('end', '');
 });
 
 server.listen(process.env.PORT || 5000, function() {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
 
-
+/*
+server.listen(5000, function() {
+  console.log('listening on *:5000');
+});
+*/
